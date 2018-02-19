ad35a729778d7d4b7399be97480ef31921193033
Matthew Houghton <matt@crandallce.com>
2018-02-18 21:45:12 -0700

    Added Math library and skeletons of the classes

---
+++ NeuralNetworkForHeigthData/Base Classes/NeuralNet.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NeuralNetworkForHeigthData.Base_Classes
+{
+    class NeuralNet
+    {
+        public void Train()
+        {
+
+        }
+        public void Calulate()
+        {
+
+        }
+        public void Seed()
+        {
+
+        }
+        public void Test()
+        {
+
+        }
+    }
+}
---
+++ NeuralNetworkForHeigthData/NetworkManager.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics.LinearAlgebra;
+
+namespace NeuralNetworkForHeigthData
+{
+    class NetworkManager
+    {
+        private int numInputs;
+
+        public int NumInputs
+        {
+            get { return numInputs; }
+            set { numInputs = value; }
+        }
+
+        private int numOutputs;
+
+        public int NumOutputs
+        {
+            get { return numOutputs; }
+            set { numOutputs = value; }
+        }
+
+        private int numHiddedNuerons;
+
+        public int NumHiddenNeurons
+        {
+            get { return numHiddedNuerons; }
+            set { numHiddedNuerons = value; }
+        }
+        private int numHiddenLayers;
+
+        public int NumHiddenLayers
+        {
+            get { return numHiddenLayers; }
+            set { numHiddenLayers = value; }
+        }
+        private Vector<double> Inputs { get; set; }
+        public Vector<double> Outputs { get; set; }
+        private Matrix<double> Hidden { get; set; } 
+        public NetworkManager(int _numIns, int _numOuts, int _numHiddenNeurons, int _numHiddenLayers)
+        {
+            Inputs
+        }
+
+
+    }
+}
